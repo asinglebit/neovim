c031327f77ee2d80e05a78c324f0172a63f7c88d
rattleworks <gasimov.abdulali@gmail.com>
2026-01-13 20:13:13 +0100

    Clipboard

--- init.lua
+++ init.lua
@@ -19,3 +19,16 @@ vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to below window" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to above window" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+local opts = { noremap = true, silent = true }
+
+-- Copy to system clipboard
+vim.keymap.set("v", "<leader>y", '"+y', opts)
+vim.keymap.set("n", "<leader>y", '"+y', opts)
+vim.keymap.set("n", "<leader>Y", '"+yg_', opts)
+vim.keymap.set("n", "<leader>yy", '"+yy', opts)
+
+-- Paste from system clipboard
+vim.keymap.set("n", "<leader>p", '"+p', opts)
+vim.keymap.set("n", "<leader>P", '"+P', opts)
+vim.keymap.set("v", "<leader>p", '"+p', opts)
+vim.keymap.set("v", "<leader>P", '"+P', opts)
