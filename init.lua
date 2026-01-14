33fa31a2347113a6dcdddae40b562bd7054f9806
rattleworks <gasimov.abdulali@gmail.com>
2026-01-14 14:51:34 +0100

    Add biome

--- init.lua
+++ init.lua
@@ -1,3 +1,9 @@
+-- Load lazy config
+require("config.lazy")
+
+-- ==============================
+-- Basic settings
+-- ==============================
 vim.cmd("syntax on")
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -6,42 +12,58 @@ vim.opt.tabstop = 4
 vim.opt.fillchars:append({ eob = " " })
 vim.o.cursorline = true
 
+-- ==============================
+-- Telescope shortcuts
+-- ==============================
 vim.keymap.set("n", "<leader>fp", "<cmd>Telescope project<cr>", { desc = "Projects" })
 
-require("config.lazy")
-
+-- ==============================
 -- Buffer navigation
+-- ==============================
 vim.keymap.set("n", "H", "<cmd>bprevious<CR>", { desc = "Previous buffer" })
 vim.keymap.set("n", "L", "<cmd>bnext<CR>", { desc = "Next buffer" })
 
+-- ==============================
 -- Window navigation
+-- ==============================
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
 vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to below window" })
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to above window" })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+
+-- ==============================
+-- System clipboard
+-- ==============================
 local opts = { noremap = true, silent = true }
 
--- Copy to system clipboard
+-- Copy
 vim.keymap.set("v", "<leader>y", '"+y', opts)
 vim.keymap.set("n", "<leader>y", '"+y', opts)
 vim.keymap.set("n", "<leader>Y", '"+yg_', opts)
 vim.keymap.set("n", "<leader>yy", '"+yy', opts)
 
--- Paste from system clipboard
+-- Paste
 vim.keymap.set("n", "<leader>p", '"+p', opts)
 vim.keymap.set("n", "<leader>P", '"+P', opts)
 vim.keymap.set("v", "<leader>p", '"+p', opts)
 vim.keymap.set("v", "<leader>P", '"+P', opts)
 
+-- ==============================
+-- Diagnostics
+-- ==============================
 vim.keymap.set("n", "<leader>j", function()
-  vim.diagnostic.open_float(nil, { focus = true, border = "rounded" })
+    vim.diagnostic.open_float(nil, { focus = true, border = "rounded" })
 end, { silent = true })
+
+-- ==============================
+-- Which-Key / nvim-silicon
+-- ==============================
 local wk = require("which-key")
 wk.add({
     mode = { "v" },  -- Only active in visual mode
-    { "<leader>s",  group = "Silicon" },  -- Group heading in Which-Key
+    { "<leader>s", group = "Silicon" },  -- Group heading in Which-Key
     { "<leader>sc", function() require("nvim-silicon").clip() end, desc = "Copy code screenshot to clipboard" },
-    { "<leader>sf", function() require("nvim-silicon").file() end,  desc = "Save code screenshot as file" },
-    { "<leader>ss", function() require("nvim-silicon").shoot() end,  desc = "Create code screenshot" },
+    { "<leader>sf", function() require("nvim-silicon").file() end, desc = "Save code screenshot as file" },
+    { "<leader>ss", function() require("nvim-silicon").shoot() end, desc = "Create code screenshot" },
 })
 
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,107 +1,147 @@
 return {
-	-- Rust: LSP and DAP via rustaceanvim
-	{
-		"mrcjkb/rustaceanvim",
-		version = "^6",
-		lazy = false,
-		dependencies = { "nvim-treesitter/nvim-treesitter", "williamboman/mason.nvim", "hrsh7th/nvim-cmp" },
-		config = function()
-			local cmp_nvim_lsp = require("cmp_nvim_lsp")
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
+    -- Rust: LSP and DAP via rustaceanvim
+    {
+        "mrcjkb/rustaceanvim",
+        version = "^6",
+        lazy = false,
+        dependencies = { "nvim-treesitter/nvim-treesitter", "williamboman/mason.nvim", "hrsh7th/nvim-cmp" },
+        config = function()
+            local cmp_nvim_lsp = require("cmp_nvim_lsp")
+            local capabilities = vim.lsp.protocol.make_client_capabilities()
+            capabilities = cmp_nvim_lsp.default_capabilities(capabilities)
 
-			vim.g.rustaceanvim = {
-				server = {
-				  on_attach = function(client, bufnr)
-					local opts = { buffer = bufnr, silent = true }
-			  
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-				  end,
-			  
-				  default_settings = {
-					["rust-analyzer"] = {
-					  cargo = { allFeatures = true },
-					  checkOnSave = true,
-					  inlayHints = { enable = true },
-					},
-				  },
-			  
-				  capabilities = capabilities,
-				},
-			  }
-		end,
-	},
-	{
-		"hrsh7th/cmp-nvim-lsp", -- for completion capabilities
-		config = function()
-			local cmp_nvim_lsp = require("cmp_nvim_lsp")
-			local capabilities = cmp_nvim_lsp.default_capabilities()
+            vim.g.rustaceanvim = {
+                server = {
+                    on_attach = function(client, bufnr)
+                        local opts = { buffer = bufnr, silent = true }
 
-			-- TypeScript: LSP via tsserver
+                        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                    end,
 
-			vim.lsp.config("ts_ls", {
-				name = "tsserver",
-    			cmd = { vim.fn.stdpath("data") .. "/mason/bin/typescript-language-server", "--stdio" },
-				capabilities = capabilities,
-				filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
-				root_dir = vim.fs.root(0, { "tsconfig.json", "package.json", ".git" }),
-				settings = {
-					typescript = {
-						format = { enable = true },
-						suggest = { completeFunctionCalls = true },
-						inlayHints = {
-							includeInlayParameterNameHints = "all",
-							includeInlayVariableTypeHints = true,
-							includeInlayFunctionLikeReturnTypeHints = true,
-						},
-					},
-					javascript = {
-						format = { enable = true },
-					},
-				},
-				on_attach = function(client, bufnr)
-					local opts = { buffer = bufnr, silent = true }
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-				end,
-			})
-			vim.lsp.enable("ts_ls")
+                    default_settings = {
+                        ["rust-analyzer"] = {
+                            cargo = { allFeatures = true },
+                            checkOnSave = true,
+                            inlayHints = { enable = true },
+                        },
+                    },
 
-		    -- Go: LSP via gopls
-		vim.lsp.config("gopls", {
-		  cmd = { vim.fn.stdpath("data") .. "/mason/bin/gopls" },
-		  filetypes = { "go", "gomod", "gowork", "gotmpl" },
-		  root_dir = vim.fs.root(0, { "go.work", "go.mod", ".git" }),
-		  capabilities = capabilities,
+                    capabilities = capabilities,
+                },
+            }
+        end,
+    },
 
-		  settings = {
-			gopls = {
-			  gofumpt = true,
-			  analyses = {
-				unusedparams = true,
-				shadow = true,
-			  },
-			  staticcheck = true,
-			},
-		  },
+    -- LSP for TypeScript, Go, Biome
+    {
+        "hrsh7th/cmp-nvim-lsp",
+        config = function()
+            local cmp_nvim_lsp = require("cmp_nvim_lsp")
+            local capabilities = cmp_nvim_lsp.default_capabilities()
+            local mason_bin = vim.fn.expand("$HOME/.local/share/nvim/mason/bin/")
 
-		  on_attach = function(client, bufnr)
-			local opts = { buffer = bufnr, silent = true }
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-		  end,
-		})
+            -- ==============================
+            -- TypeScript / JavaScript LSP
+            -- ==============================
+            vim.lsp.config("ts_ls", {
+                name = "tsserver",
+                cmd = { mason_bin .. "typescript-language-server", "--stdio" },
+                capabilities = capabilities,
+                filetypes = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
+                root_dir = vim.fs.root(0, { "tsconfig.json", "package.json", ".git" }),
+                settings = {
+                    typescript = {
+                        format = { enable = true },
+                        suggest = { completeFunctionCalls = true },
+                        inlayHints = {
+                            includeInlayParameterNameHints = "all",
+                            includeInlayVariableTypeHints = true,
+                            includeInlayFunctionLikeReturnTypeHints = true,
+                        },
+                    },
+                    javascript = { format = { enable = true } },
+                },
+                on_attach = function(client, bufnr)
+                    local opts = { buffer = bufnr, silent = true }
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                end,
+            })
+            vim.lsp.enable("ts_ls")
 
-		vim.lsp.enable("gopls")
+            -- ==============================
+            -- Go LSP
+            -- ==============================
+            vim.lsp.config("gopls", {
+                cmd = { mason_bin .. "gopls" },
+                filetypes = { "go", "gomod", "gowork", "gotmpl" },
+                root_dir = vim.fs.root(0, { "go.work", "go.mod", ".git" }),
+                capabilities = capabilities,
+                settings = {
+                    gopls = {
+                        gofumpt = true,
+                        analyses = { unusedparams = true, shadow = true },
+                        staticcheck = true,
+                    },
+                },
+                on_attach = function(client, bufnr)
+                    local opts = { buffer = bufnr, silent = true }
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+                end,
+            })
+            vim.lsp.enable("gopls")
 
-		end,
-	},
+            -- ==============================
+            -- Biome LSP
+            -- ==============================
+            local function on_attach_biome(client, bufnr)
+                local opts = { buffer = bufnr, silent = true }
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+
+                vim.api.nvim_create_autocmd("BufWritePre", {
+                    group = vim.api.nvim_create_augroup("BiomeFixAll", { clear = true }),
+                    buffer = bufnr,
+                    callback = function()
+                        vim.lsp.buf.code_action({
+                            context = { only = { "source.fixAll.biome" }, diagnostics = {} },
+                            apply = true,
+                        })
+                    end,
+                })
+            end
+
+            vim.lsp.config("biome", {
+                cmd = { mason_bin .. "biome", "lsp-proxy" },
+                filetypes = {
+                    "htmlangular", "html",
+                    "javascript", "javascriptreact", "javascript.jsx",
+                    "typescript", "typescriptreact", "typescript.tsx",
+                    "css", "scss",
+                },
+                root_markers = {
+                    ".eslintrc", ".eslintrc.js", ".eslintrc.cjs",
+                    ".eslintrc.yaml", ".eslintrc.yml", ".eslintrc.json",
+                    "eslint.config.js", "eslint.config.mjs", "eslint.config.cjs",
+                    "eslint.config.ts", "eslint.config.mts", "eslint.config.cts",
+                    "biome.json", "package.json", ".git",
+                },
+                on_attach = on_attach_biome,
+            })
+            vim.lsp.enable("biome")
+        end,
+    },
 }
+
